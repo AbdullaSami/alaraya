5d3fb384c2df49d69997f69a6dfa91c5a9cfe3d2
AbdullaSami <alattariabdulla@gmail.com>
2026-02-04 14:25:08 +0200

    fix validation to test

--- app/Http/Controllers/ShipOrderDataController.php
+++ app/Http/Controllers/ShipOrderDataController.php
@@ -65,13 +65,13 @@ class ShipOrderDataController extends Controller
                 'policies' => 'required_without:bookings|array|min:1',
                 'policies.*.policy_number' => 'required|string|unique:ship_policies,policy_number',
                 'policies.*.containers' => 'required|array|min:1',
-                'policies.*.containers.*.container_number' => 'required|string|unique:ship_containers_details,container_number',
+                'policies.*.containers.*.container_number' => 'required|string',
 
                 // Ship Bookings (array)
                 'bookings' => 'required_without:policies|array|min:1',
                 'bookings.*.booking_number' => 'required|string|unique:ship_bookings,booking_number',
                 'bookings.*.containers' => 'required|array|min:1',
-                'bookings.*.containers.*.container_number' => 'required|string|unique:ship_containers_details,container_number',
+                'bookings.*.containers.*.container_number' => 'required|string',
 
                 // Ship Contact Data
                 'contact_loading_name' => 'required|string',
