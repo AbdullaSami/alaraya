f946e27ddf694de9b86181103468592282122602
AbdullaSami <alattariabdulla@gmail.com>
2026-03-14 08:55:05 +0200

    fix validation typo

--- app/Http/Controllers/TreasuryController.php
+++ app/Http/Controllers/TreasuryController.php
@@ -30,7 +30,7 @@ class TreasuryController extends Controller
             $validatedData = $request->validate([
                 'is_main' => 'required|boolean',
                 'name' => 'required|string|max:255',
-                'balance' => 'nullable|decimal',
+                'balance' => 'nullable|decimal:0,2',
             ]);
             $treasury = Treasury::create($validatedData);
             return response()->json($treasury, 201);
