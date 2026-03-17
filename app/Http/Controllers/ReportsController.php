c0def2093553f65211c17dfaf2a408a584667397
AbdullaSami <alattariabdulla@gmail.com>
2026-03-17 23:47:32 +0200

    tset

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -214,7 +214,6 @@ class ReportsController extends Controller
                 'success' => true,
                 'data' => [
                     'ship_orders_count' => $totalShipOrdersCount,
-                    'policies' => $query->policies,
                     'operating_orders' => $totalOperatingOrders,
                     'total_orders_noloans' => $totalOrdersNoloans,
                     'ship_orders_details' => $shipOrdersDetails,
