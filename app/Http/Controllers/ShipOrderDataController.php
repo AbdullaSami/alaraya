fec4093913daa832058c28011e3cfc3901e88254
AbdullaSami <alattariabdulla@gmail.com>
2026-03-17 22:56:26 +0200

    add the right function to save the order treasury

--- app/Http/Controllers/ShipOrderDataController.php
+++ app/Http/Controllers/ShipOrderDataController.php
@@ -139,7 +139,7 @@ class ShipOrderDataController extends Controller
                     'transfers_count' => $validatedData['transfers_count'] ?? 1,
                 ]);
 
-                $shipOrderData->orderTreasury()->sync($validatedData['treasury_id']);
+                $shipOrderData->orderTreasury()->save($validatedData['treasury_id']);
                 // Create Ship Line Client
                 $shipLineClient = ShipLineClient::create([
                     'ship_order_data_id' => $shipOrderData->id,
