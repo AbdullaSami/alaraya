e69f86c5411ebec853deba3631352d5d2148a56f
AbdullaSami <alattariabdulla@gmail.com>
2026-03-24 14:52:01 +0200

    test

--- app/Http/Controllers/TreasuryOperationsController.php
+++ app/Http/Controllers/TreasuryOperationsController.php
@@ -186,7 +186,7 @@ class TreasuryOperationsController extends Controller
             $shiftHandle = new TreasuryShiftHandle();
             $shiftHandle->treasury_id = $validatedData['treasury_id'];
             $shiftHandle->user_id = $validatedData['user_id'];
-            $shiftHandle->amount = $treasury->amount ?? 0;
+            $shiftHandle->balance = $treasury->amount ?? 0;
             $shiftHandle->action = $validatedData['action'];
             $shiftHandle->save();
 
