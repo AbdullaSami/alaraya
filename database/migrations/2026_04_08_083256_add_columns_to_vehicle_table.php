5feeb71b41824c00eeeaa7ef779ad71dcc2e7bec
abdulla-samii <abdulla.sami@splendidad.com>
2026-04-08 13:59:01 +0200

    fix typo

--- database/migrations/2026_04_08_083256_add_columns_to_vehicle_table.php
+++ database/migrations/2026_04_08_083256_add_columns_to_vehicle_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('vehicle', function (Blueprint $table) {
+        Schema::table('vehicles', function (Blueprint $table) {
             $table->string('type')->nullable();
             $table->string('office_name')->nullable();
         });
